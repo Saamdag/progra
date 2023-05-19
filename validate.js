66312eeb622e1c9cfea113fa9206ce153683baff
Saamdag <samirbrusasca@outlook.com>
2023-05-19 18:03:13 -0300

    fir

---
+++ validate.js
@@ -0,0 +1,11 @@
+$("#formPrincipal").validate({
+    rules:{}
+})
+
+$("submit").click(function(){
+    let nombre = $("nombre").val()
+    let apellido = $("apellido").val()
+    let email = $("email").val()
+
+    
+})
